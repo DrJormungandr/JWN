c60e5c2f1c1b8a352a04081e2150b8ea4bbb6111
DrJormungandr <ouroborosjormungand@gmail.com>
2019-10-05 14:09:37 +0700

    second commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool isOnGound = true;
+    public float speed = 20;
+    public float jumpForce = 3;
+    private float horizontalInput;
+    private SpriteRenderer playerSprite;
+    private Rigidbody2D playerRb;
+    private Animator playerAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
+        playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal") * speed;
+        transform.Translate(Vector2.right * Time.deltaTime * horizontalInput);
+       // playerDirection(horizontalInput);
+        jump();
+        
+    }
+
+  /*  private void playerDirection(float direction)
+    {
+        
+        if (direction < 0)
+        {
+            playerAnim.SetBool("isRunning", true);
+            playerSprite.flipX = true;
+        }
+        else if (direction > 0)
+        {
+            playerAnim.SetBool("isRunning", true);
+            playerSprite.flipX = false;
+        }
+        else
+        {
+            playerAnim.SetBool("isRunning", false);
+        }
+    } */
+
+    private void jump()
+    {
+        if (Input.GetButtonDown("Jump") && isOnGound)
+        {
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isOnGound = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGound = true;
+        }
+    }
+}
