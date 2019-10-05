3e5cf2a9b3e32769fdb124ee020978affa888061
DrJormungandr <ouroborosjormungand@gmail.com>
2019-10-05 23:20:02 +0700

    Added close combat physics and enemyHealth

--- Assets/Scripts/CloseCombat.cs
+++ Assets/Scripts/CloseCombat.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class CloseCombat : MonoBehaviour
 {
     // Start is called before the first frame update
-   
+    public int damage = 1;
+    private Rigidbody2D otherRb;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,6 +20,17 @@ public class CloseCombat : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-     
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            otherRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            otherRb.AddForce(new Vector2(5, 3), ForceMode2D.Impulse);
+            EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+            enemyHealth.health -= damage;
+
+        }
     }
 }
