144377ac4ae6f1f147f93ab23239ef4befcd5a15
MagistrYoda9 <amedved200222@yandex.ru>
2019-10-06 12:00:51 +0700

    lvl design and music

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     public int health = 2;
+    private Animator enemyAnim;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,7 @@ public class EnemyHealth : MonoBehaviour
     {
         if (health < 1)
         {
+            enemyAnim.SetTrigger("death");
             Destroy(gameObject);
         }
     }
