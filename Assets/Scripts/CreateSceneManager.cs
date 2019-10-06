85e19b9aaa950d30938bc7ae65cbee259799ed50
MagistrYoda9 <amedved200222@yandex.ru>
2019-10-06 17:14:12 +0700

    a

--- Assets/Scripts/CreateSceneManager.cs
+++ Assets/Scripts/CreateSceneManager.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class CreateSceneManager : MonoBehaviour
 {
     // Start is called before the first frame update
