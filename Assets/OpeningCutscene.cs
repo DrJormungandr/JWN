c35e9008377b5b32d963d3f61c736d230b5d482f
MagistrYoda9 <amedved200222@yandex.ru>
2019-10-06 13:49:23 +0700

    smth

---
+++ Assets/OpeningCutscene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class OpeningCutscene : MonoBehaviour {
+    public bool isLoad = false;
+
+
+    void Update()
+    {
+   
+    if(isLoad == true)
+    {
+        SceneManager.LoadScene(1);
+    }
+                
+    }
+}
--- Assets/Scripts/EnemyMonkGeniusAI.cs
+++ Assets/Scripts/EnemyMonkGeniusAI.cs
@@ -30,5 +30,7 @@ public class EnemyMonkGeniusAI : MonoBehaviour
         {
             transform.Translate(awayFromPlayer * Time.deltaTime * speed);
         }
+
+
     }
 }
