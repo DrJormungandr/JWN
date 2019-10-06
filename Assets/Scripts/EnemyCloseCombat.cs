aa2c4b2f65bfe5488f957cd3b01dfce33befc6c7
DrJormungandr <ouroborosjormungand@gmail.com>
2019-10-06 09:54:40 +0700

    added close enemy knight AI

---
+++ Assets/Scripts/EnemyCloseCombat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCloseCombat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int damage = 1;
+    private Rigidbody2D otherRb;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            otherRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            otherRb.AddForce(new Vector2(5, 3), ForceMode2D.Impulse);
+            EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+            enemyHealth.health -= damage;
+
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyKnightGeniusAI.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyKnightGeniusAI : MonoBehaviour
+{
+    private GameObject player;
+    private Animator knightAnim;
+    private Vector3 offset;
+    public GameObject hurtbox;
+    bool looksRight;
+    private float distanceFromPlayer;
+    public float sightDistance = 8;
+    public float attackDistance = 2;
+    public float speed = 3;
+    private float attackDelay = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        knightAnim = gameObject.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceFromPlayer = Mathf.Abs(player.transform.position.x - transform.position.x);
+        PlayerSight();
+    }
+
+    private void PlayerSight()
+    {
+        Debug.Log(distanceFromPlayer.ToString());
+        Vector3 awayFromPlayer = new Vector3(transform.position.x + player.transform.position.x, transform.position.y);
+        if (distanceFromPlayer < sightDistance)
+        {
+            transform.Translate(awayFromPlayer * Time.deltaTime * 2);
+        }
+    }
+
+    private void AttackPlayer()
+    {
+        if (distanceFromPlayer <= attackDistance)
+        {
+            offset = new Vector3(2, -0.2F, 0);
+            if (looksRight)
+            {
+                offset.x *= -1;
+            }
+            else
+            {
+                offset.x = Mathf.Abs(offset.x);
+            }
+
+            offset = gameObject.transform.position - offset;
+            if (Time.time > 1)
+            {
+                Instantiate(hurtbox, (offset), gameObject.transform.rotation);
+            }
+            knightAnim.SetTrigger("attackAction");
+        }
+    }
+}
+
+
