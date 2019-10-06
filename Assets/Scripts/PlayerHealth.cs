201f548eda586789828205090471b7d1549156d4
MagistrYoda9 <amedved200222@yandex.ru>
2019-10-06 08:45:13 +0700

    added a script for player health and UI

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+
+    // Start is called before the first frame update
+
+
+    void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+            if (health < 1)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
